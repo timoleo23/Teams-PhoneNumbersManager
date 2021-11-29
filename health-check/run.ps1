0c5dda2038c97a2ead90dc2eb6eb12e69f7c171e
timoleo23 <timoleo23@users.noreply.github.com>
2021-11-29 16:08:20 +0100

    Remove AzureAD Module

--- health-check/run.ps1
+++ health-check/run.ps1
@@ -6,9 +6,11 @@ param($Request, $TriggerMetadata)
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
+#Get-Module -ListAvailable | Select-Object Name, Path
+
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
     ContentType = 'application/json'
-    Body = "OK"
+    Body = $(Get-Module -ListAvailable | Select-Object Name, Path)
 })
--- profile.ps1
+++ profile.ps1
@@ -20,8 +20,8 @@
 # Enable-AzureRmAlias
 
 # You can also define functions or aliases that can be referenced in any of your PowerShell functions.
-$AzureADModulePath=".\Modules\AzureAD\2.0.2.140\AzureAD.psd1"
-Import-Module $AzureADModulePath -UseWindowsPowerShell
+#$AzureADModulePath=".\Modules\AzureAD\2.0.2.140\AzureAD.psd1"
+#Import-Module $AzureADModulePath -UseWindowsPowerShell
 
-$SkypeOnlineModulePath=".\Modules\MicrosoftTeams\2.6.0\MicrosoftTeams.psd1"
-Import-Module $SkypeOnlineModulePath
+#$SkypeOnlineModulePath=".\Modules\MicrosoftTeams\2.6.0\MicrosoftTeams.psd1"
+#Import-Module $SkypeOnlineModulePath
