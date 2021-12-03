c46f3884111449f21aca6d53723f00d26959f8d5
timoleo23 <timoleo23@users.noreply.github.com>
2021-12-03 15:39:17 +0100

    Update health check

--- health-check/run.ps1
+++ health-check/run.ps1
@@ -5,26 +5,22 @@ param($Request, $TriggerMetadata)
 
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
-$StatusCode =  [HttpStatusCode]::BadGateway
-$Resp = "OK"
 
-If ($StatusCode -eq [HttpStatusCode]::OK) {
-    Try {
-        Connect-MicrosoftTeams -Credential $Credential -ErrorAction:Stop
-        Connect-AzureAD -Credential $Credential -ErrorAction:Stop
-    }
-    Catch {
-        $Resp = @{ "Error" = $_.Exception.Message }
-        $StatusCode =  [HttpStatusCode]::BadGateway
-    }
-}
+#     Try {
+#         Connect-MicrosoftTeams -Credential $Credential -ErrorAction:Stop
+#         Connect-AzureAD -Credential $Credential -ErrorAction:Stop
+#     }
+#     Catch {
+#         $Resp = @{ "Error" = $_.Exception.Message }
+#         $StatusCode =  [HttpStatusCode]::BadGateway
+#     }
 
-Disconnect-MicrosoftTeams
-Disconnect-AzureAD
+# Disconnect-MicrosoftTeams
+# Disconnect-AzureAD
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = $StatusCode
+    StatusCode = [HttpStatusCode]::OK
     ContentType = 'application/json'
-    Body = $Resp
+    Body = "OK"
 })
