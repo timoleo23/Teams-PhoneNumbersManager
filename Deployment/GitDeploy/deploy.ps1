b9475c3bfc49d207883265bdc5524122b6dcba21
timoleo23 <timoleo23@users.noreply.github.com>
2021-12-05 17:13:37 +0100

    Move function files into FunctionApp folder

--- Deployment/GitDeploy/deploy.ps1
+++ Deployment/GitDeploy/deploy.ps1
@@ -1,4 +1,4 @@
-$Rgname = "teams-test"
+$Rgname = "teams-test2"
 
 New-AzResourceGroup -Name $Rgname -Location westeurope
 New-AzResourceGroupDeployment -ResourceGroupName $Rgname -TemplateFile .\Deployment\GitDeploy\azuredeploy.json -TemplateParameterFile .\Deployment\GitDeploy\parameters-local.json
