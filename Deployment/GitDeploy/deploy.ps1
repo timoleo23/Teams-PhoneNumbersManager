4c84659d4b020b0a9754590337196a09942e9656
timoleo23 <timoleo23@users.noreply.github.com>
2021-12-05 17:28:08 +0100

    Change ARM deploy URL

--- Deployment/GitDeploy/deploy.ps1
+++ Deployment/GitDeploy/deploy.ps1
@@ -1,4 +1,4 @@
-$Rgname = "teams-test2"
+$Rgname = "teams-test3"
 
 New-AzResourceGroup -Name $Rgname -Location westeurope
 New-AzResourceGroupDeployment -ResourceGroupName $Rgname -TemplateFile .\Deployment\GitDeploy\azuredeploy.json -TemplateParameterFile .\Deployment\GitDeploy\parameters-local.json
