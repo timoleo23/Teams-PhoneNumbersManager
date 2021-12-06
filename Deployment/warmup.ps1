4e0f750d3135aa931229cf77e42dd2557f2415f3
timoleo23 <timoleo23@users.noreply.github.com>
2021-12-06 17:25:09 +0100

    Changing the P1v2 sku

--- Deployment/warmup.ps1
+++ Deployment/warmup.ps1
@@ -1,4 +1,3 @@
-[CmdletBinding()]
 Param(
     [Parameter(Mandatory=$true,HelpMessage="You must enter function hostname with argument -hostname [host URI]")][string]$hostname,
     [Parameter(Mandatory=$true,HelpMessage="You must enter function code with argument -code [function code]")][string]$code,
@@ -21,33 +20,43 @@ function generateConfig ([string]$hostname,[string]$code,[string]$UPN,[int]$work
 }
 
 function checkStatus($jobStatus) {
-    $result = "OK"
     foreach ($item in $jobStatus) {
-        if ($item.StatusCode -ne 200) {$result = "NOK"}
+        if ( ($item.StatusCode -ne 200) -OR ([string]::IsNullOrWhiteSpace($item.StatusCode))) { 
+            return $false
+        }
     }
-    return $result
+    return $true
 }
 
 $retries = 0
 Do
 {
-    Write-Host "Function warm-up started at" $(Get-Date) "- Retry #"$retries
+    Write-Host "Function warm-up started at" $(Get-Date) "- Attempt #" ($retries+1)
     $job = generateConfig $hostname $code $upn $workers $retries | ForEach-Object -ThrottleLimit $workers -Parallel { 
         $timeout = 120
         $start = Get-Date
         $Result = Invoke-WebRequest -URI $_.URI -Method 'Get' -TimeoutSec $timeout -MaximumRetryCount 1
         $finish = Get-Date
         $duration = ($finish - $start).TotalSeconds
-        $Resp = New-Object -TypeName psobject -Property @{Duration= [Math]::Round($duration,2); StatusCode= $Result.StatusCode; StatusDescription= If($duration -gt $timeout) {"Request timed out ($timeout sec)"} Else {$Result.StatusDescription};TriggerTime= (Get-Date -DisplayHint Time)}
+        $Resp = New-Object -TypeName psobject -Property @{Duration= [Math]::Round($duration,2); StatusCode= $Result.StatusCode; StatusDescription= If($duration -gt $timeout) {"Request timed out ($timeout sec)"} Else {$Result.StatusDescription};TriggerTime= (Get-Date -DisplayHint Time);WorkerId=$_.ID}
         return $Resp
     } -AsJob
-    $jobresults = $job | Wait-Job | Receive-Job
+    $jobresult = $job | Wait-Job | Receive-Job
+    $test = checkStatus($jobresults)
+
+    $jobresults += $jobresult
     $job | Remove-Job
+    If ($retries -ne 0) {
+        Write-Host "Sleeping for 60s before retrying"
+        Start-Sleep -Seconds 60
+    }
     $retries +=1
 }
-until( (checkStatus($jobresults) -eq "OK") -OR ($retries -ge $maxRetry))
+until( ($test -EQ $TRUE) -OR ($retries -ge $maxRetry))
 
 If ($retries -lt $maxRetry) {
     Write-Host "Reached max retries - Function app still not warmed up - Please restart the script or check error messages"
 }
 $jobresults | Select-Object TriggerTime,Duration,StatusCode,StatusDescription | Sort-Object TriggerTime 
+
+
