8870b6a641e7dcf6dd9f4650f54f7a006f10058d
timoleo23 <timoleo23@users.noreply.github.com>
2022-05-23 10:20:09 +0200

    Project migrated to OfficeDev

--- Deployment/GitDeploy/deploy.ps1
+++
@@ -1,5 +0,0 @@
-$Rgname = "Teams-Telephony-Manager"
-$Rgregion = "westeurope"
-
-New-AzResourceGroup -Name $Rgname -Location $Rgregion
-New-AzResourceGroupDeployment -ResourceGroupName $Rgname -TemplateFile .\Deployment\GitDeploy\azuredeploy.json -TemplateParameterFile .\Deployment\GitDeploy\parameters-local.json
